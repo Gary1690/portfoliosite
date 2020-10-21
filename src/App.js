9e39b5f618df4aa682b0a815168769e3f2937908
Gary Cordero <GaryCordero1690@gmail.com>
2020-10-21 14:10:48 -0400

    Changing approach

--- src/App.js
+++ src/App.js
@@ -1,29 +1,35 @@
 import React from 'react';
 import './App.css';
 import {Layout,Header,Navigation,Drawer,Content} from 'react-mdl'
-
+import Main from './components/main'
+import {Link} from 'react-router-dom'
+ 
 function App() {
   return (
     <div className="App">
         <div >
-          <Layout style={{background: 'url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover'}}>
-              <Header transparent title="Title" style={{color: 'white'}}>
+          <Layout >
+              <Header className="header-color" title=" " style={{color: 'white'}}>
                   <Navigation>
-                      <a href="#">Link</a>
-                      <a href="#">Link</a>
-                      <a href="#">Link</a>
-                      <a href="#">Link</a>
+                      <Link to="/">Landing Page</Link>
+                      <Link to="/about">About</Link>
+                      <Link to="/resume">Resume</Link>
+                      <Link to="/projects">Projects</Link>
+                      <Link to="contact">Contact</Link>
                   </Navigation>
               </Header>
               <Drawer title="Menu">
                   <Navigation>
-                      <a href="#">Link</a>
-                      <a href="#">Link</a>
-                      <a href="#">Link</a>
-                      <a href="#">Link</a>
+                      <Link to="/">Landing Page</Link>
+                      <Link to="/about">About</Link>
+                      <Link to="/resume">Resume</Link>
+                      <Link to="/projects">Projects</Link>
+                      <Link to="contact">Contact</Link>
                   </Navigation>
               </Drawer>
-              <Content />
+              <Content >
+                <Main/> 
+              </Content>
           </Layout>
       </div>
     </div>
--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,10 +1,33 @@
 import React from 'react'
+import Typical from 'react-typical'
+import { Cell, Grid } from 'react-mdl'
 
 const LandingPage = () => {
   return (
-    <div>
-      <h1>Landing Page</h1>
-    </div>
+    <section style = {{width:'100%',margin:'auto',  backgroundColor: 'blue'}}>
+       <div>
+         <h1> Hi I am  <strong>Gary Cordero</strong></h1>
+          <p>
+            I am a 
+            <Typical
+              loop = {Infinity}
+              wrapper = "b"
+              steps = {[
+                " Developer",
+                1000,
+                " Problem Solver",
+                1000,
+                " Gamer",
+                1000,
+                " Avid Reader",
+                1000,
+                " Foodie",
+                1000
+              ]}
+            />
+          </p>
+       </div>
+    </section>
   )
 }
 
